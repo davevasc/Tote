ed564ba97753b95e282261f4c61de44c17e7f817
dave <daveplus@gmail.com>
2022-01-20 20:28:20 +0100

    Initial Commit

---
+++ Tote/ToteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToteApp.swift
+//  Tote
+//
+//  Created by David Vicente on 20/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ToteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
