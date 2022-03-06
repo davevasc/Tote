439ce11ab436b35fbefa4019dc652278a89e26f4
dave <daveplus@gmail.com>
2022-03-06 13:47:03 +0100

    comentario en conf firebase

--- Tote/ToteApp.swift
+++ Tote/ToteApp.swift
@@ -14,12 +14,14 @@ struct ToteApp: App {
     // Init Firebase
     init() {
         FirebaseApp.configure()
+        // esto se tiene que borrar tambien
     }
     
     // Subida de mac al beta
     // subida solo a BETA
     // esto se tiene que borrar de local
     // Borrar de local pero accediendo a beta
+    // BORRAR DE LOCAL BORRAR DE LOCAL
     
     var body: some Scene {
         WindowGroup {
